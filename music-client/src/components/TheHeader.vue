4fcdf1034857bcbfe5d5c7cb319052d81d8f0433
lizhi <lizhi@internetapi.cn>
2021-09-18 14:27:34 +0800

    commit and test

--- music-client/src/components/TheHeader.vue
+++ music-client/src/components/TheHeader.vue
@@ -74,6 +74,7 @@ import { navMsg, loginMsg, menuList, dropBuyMsg, dropUploadMsg } from '../assets
 import { ICON } from '../assets/icon/index'
 
 export default {
+
   name: 'the-header',
   mixins: [mixin],
   data () {
@@ -143,8 +144,17 @@ export default {
       }
     },
     goSearch () {
-      this.$store.commit('setSearchword', this.keywords)
-      this.$router.push({path: '/search', query: {keywords: this.keywords}})
+      var xmlhttp = new XMLHttpRequest()
+      var url = 'http://82.157.177.72:8082/movie-nft-servermovie/search?keyWord=%E9%A3%8E%E4%BA%91'
+      var type = 'GET'// 方法
+      xmlhttp.open(type, url, true)// 方法，接口，异步
+      xmlhttp.send()// 发送请求
+      xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.status === 200 && xmlhttp.readyState === 4) {
+          var result = JSON.parse(xmlhttp.response) // 获取到的json数据
+          console.log(result)
+        }
+      }
     }
   }
 }
